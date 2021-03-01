e62adfac237b764b1f3cc2f805a274420fcb246c
HirokiYasui <hiromaily2@gmail.com>
2021-03-01 22:58:53 +0900

    add jwt middleware

---
+++ pkg/goa/middleware/middleware.go
@@ -0,0 +1,72 @@
+package middleware
+
+import (
+	"github.com/hiromaily/go-goa/pkg/jwts"
+	"github.com/pkg/errors"
+	"go.uber.org/zap"
+	"net/http"
+	"strings"
+)
+
+// Middlewarer interface
+type Middlewarer interface {
+	CheckJWT() func(http.Handler) http.Handler
+}
+
+// middleware object
+type middleware struct {
+	logger *zap.Logger
+	jwter  jwts.JWTer
+}
+
+// NewMiddleware returns Server interface
+func NewMiddleware(
+	logger *zap.Logger,
+	jwter jwts.JWTer,
+) Middlewarer {
+	return &middleware{
+		logger: logger,
+		jwter:  jwter,
+	}
+}
+
+func (m *middleware) CheckJWT() func(http.Handler) http.Handler {
+	return func(h http.Handler) http.Handler {
+		// A HTTP handler is a function.
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			//token := jwt.ContextJWT(ctx)
+			//
+			//if val, ok := token.Claims.(jwtgo.MapClaims)["scopes"].(string); !ok {
+			//	w.WriteHeader(401)
+			//	return jwt.ErrJWTError("Token is expired or not valid")
+			//} else if val != "api:access" {
+			//	return jwt.ErrJWTError("you are not allowed to access api")
+			//}
+			//if val, ok := token.Claims.(jwtgo.MapClaims)["user"].(float64); ok {
+			//	//fmt.Println("token.Claims[user]:", val)
+			//	ctx = context.WithValue(ctx, "user.jwt", int(val))
+			//}
+
+			m.logger.Info("middleware CheckJWT")
+
+			auth := r.Header.Get("Authorization")
+			if auth == "" {
+				m.logger.Error("http.StatusBadRequest", zap.Error(errors.New("Authorization header is missing")))
+				return
+			}
+
+			// Bearer token
+			authParts := strings.Split(auth, " ")
+			if len(authParts) != 2 || authParts[0] != "Bearer" {
+				m.logger.Error("http.StatusBadRequest", zap.Error(errors.New("Authorization header is invalid")))
+				return
+			}
+			if err := m.jwter.ValidateToken(authParts[1]); err != nil {
+				m.logger.Error("http.StatusBadRequest", zap.Error(err))
+				return
+			}
+
+			h.ServeHTTP(w, r)
+		})
+	}
+}
