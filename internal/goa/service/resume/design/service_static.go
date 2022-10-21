b7e3976d6b1fb4655acb90fe5b9aa1cb936baa40
HirokiYasui <hiromaily2@gmail.com>
2022-10-22 00:24:15 +0900

    refactoring

--- internal/goa/service/resume/design/service_static.go
+++ internal/goa/service/resume/design/service_static.go
@@ -12,28 +12,7 @@ var _ = Service("static", func() {
 		cors.Methods("GET", "OPTIONS")
 	})
 	Files("/assets/{*filepath}", "assets/")
-	Files("/swagger-ui/*filepath", "resources/swagger-ui/dist/")
+	//Files("/openapi.json", "../gen/http/openapi.json")
+	//Files("/swagger-ui/*filepath", "resources/swagger-ui/dist/")
 	//Files("/swagger.json", "goa/swagger/swagger.json")
-	Files("/openapi.json", "../gen/http/openapi.json")
 })
-
-//var _ = Resource("public", func() {
-//	Origin("*", func() {
-//		Methods("GET", "OPTIONS")
-//	})
-//	Files("/", "public/index.html")
-//})
-
-//var _ = Resource("js", func() {
-//	Origin("*", func() {
-//		Methods("GET", "OPTIONS")
-//	})
-//	Files("/js/*filepath", "public/js")
-//})
-
-//var _ = Resource("swagger", func() {
-//	Origin("*", func() {
-//		Methods("GET", "OPTIONS")
-//	})
-//	Files("/swagger.json", "public/swagger/swagger.json")
-//})
