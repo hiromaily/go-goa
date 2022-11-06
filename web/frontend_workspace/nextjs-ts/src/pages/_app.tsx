7a2b5c3dd4ec0db2eef67bdf913ccbbf8edf83a6
HirokiYasui <hiromaily2@gmail.com>
2022-11-06 21:55:53 +0900

    work on Prop className did not match issue

--- web/frontend_workspace/nextjs-ts/src/pages/_app.tsx
+++ web/frontend_workspace/nextjs-ts/src/pages/_app.tsx
@@ -1,26 +1,44 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import type { EmotionCache } from '@emotion/cache'
+import { CacheProvider } from '@emotion/react'
 import CssBaseline from '@mui/material/CssBaseline'
 import { ThemeProvider } from '@mui/material/styles'
+//import PropTypes from 'prop-types'
 import { useDarkMode } from 'usehooks-ts'
 import { monotoneTheme } from '../theme/theme'
+import createEmotionCache from '../utils/createEmotionCache'
 
 if (process.env.NODE_ENV === 'development' || process.env.NODE_ENV === 'test') {
   const mockServer = () => import('@/mocks/worker')
   mockServer()
 }
 
-function MyApp({ Component, pageProps }: AppProps) {
+const clientSideEmotionCache = createEmotionCache()
+
+interface MyAppProps extends AppProps {
+  emotionCache?: EmotionCache
+}
+
+function MyApp({ Component, emotionCache = clientSideEmotionCache, pageProps }: MyAppProps) {
   const { isDarkMode } = useDarkMode()
   // switch theme by isDarkMode Flag
   const theme = monotoneTheme(isDarkMode)
 
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
   )
 }
+
+// MyApp.propTypes = {
+//   Component: PropTypes.elementType.isRequired,
+//   emotionCache: PropTypes.object,
+//   pageProps: PropTypes.object.isRequired,
+// };
 
 export default MyApp
--- web/frontend_workspace/nextjs-ts/src/pages/_document.tsx
+++ web/frontend_workspace/nextjs-ts/src/pages/_document.tsx
@@ -1,19 +1,79 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import Document, {
+  DocumentContext,
+  DocumentProps,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from 'next/document'
+import * as React from 'react'
+import { EmotionJSX } from '@emotion/react/types/jsx-namespace'
+import createEmotionServer from '@emotion/server/create-instance'
+import createEmotionCache from '../utils/createEmotionCache'
 
-export default function Document() {
-  return (
-    <Html>
-      <Head>
-        <link
-          rel='stylesheet'
-          href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
-        />
-        <link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons' />
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+// Refer to
+// - https://github.com/search?q=getStaticProps+createEmotionServer&type=code
+// - https://github.com/aumapichai/Storymap/blob/eb0ae7f4677a4daa022513cf0387e5abbddd33cd/src/pages/_document.tsx
+// - https://github.com/VityaSchel/the-archive/blob/4bbb07748275ba60ffb446c7be1250bcad5b528f/pages/_document.tsx
+// - https://github.com/oubakiou/100perts/blob/58bf550eb5778079ca2d7b809f0cd51a64cb65d1/v0.1.1/helloworld-app/docs/chapter3.md
+//   - Material-UI を導入してみようの項
+
+type MyDocumentProps = DocumentProps & {
+  emotionStyleTags: EmotionJSX.Element[]
 }
+
+const MyDocument = (props: MyDocumentProps) => (
+  <Html>
+    <Head>
+      <link
+        rel='stylesheet'
+        href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
+      />
+      <link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons' />
+      {props.emotionStyleTags}
+    </Head>
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+)
+
+// https://github.com/mui/material-ui/blob/master/examples/nextjs/pages/_document.js
+MyDocument.getStaticProps = async (ctx: DocumentContext) => {
+  // eslint-disable-next-line  testing-library/render-result-naming-convention
+  const originalRenderPage = ctx.renderPage
+
+  const cache = createEmotionCache()
+  const { extractCriticalToChunks } = createEmotionServer(cache)
+
+  /* eslint-disable */
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: (App) =>
+        function EnhanceApp(props) {
+          // FIXME: error by `tsc --noEmit`
+          return <App emotionCache={cache} {...props} />
+        },
+    })
+  /* eslint-enable */
+
+  const initialProps = await Document.getInitialProps(ctx)
+
+  const emotionStyles = extractCriticalToChunks(initialProps.html)
+  const emotionStyleTags = emotionStyles.styles.map((style) => (
+    <style
+      data-emotion={`${style.key} ${style.ids.join(' ')}`}
+      key={style.key}
+      // eslint-disable-next-line react/no-danger
+      dangerouslySetInnerHTML={{ __html: style.css }}
+    />
+  ))
+
+  return {
+    ...initialProps,
+    emotionStyleTags,
+  }
+}
+
+export default MyDocument
