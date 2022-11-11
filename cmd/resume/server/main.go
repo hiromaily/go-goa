18d2b6f0e2b188fa582e1d1cb91a9f07665fd165
HirokiYasui <hiromaily2@gmail.com>
2022-11-11 20:30:42 +0900

    fix listen address on server

--- cmd/resume/server/main.go
+++ cmd/resume/server/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"flag"
 	"fmt"
-	"net"
 	"net/url"
 	"os"
 	"os/signal"
@@ -12,6 +11,7 @@ import (
 	"syscall"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/pkg/errors"
 	"github.com/rs/zerolog/log"
 
 	"github.com/hiromaily/go-goa/pkg/config"
@@ -29,12 +29,12 @@ func main() {
 	// Define command line flags, add any other flag required to configure the
 	// service.
 	var (
-		hostF     = flag.String("host", "localhost", "Server host (valid values: localhost)")
-		domainF   = flag.String("domain", "", "Host domain name (overrides host domain specified in service design)")
-		httpPortF = flag.String("http-port", "", "HTTP port (overrides host HTTP port specified in service design)")
-		secureF   = flag.Bool("secure", false, "Use secure scheme (https or grpcs)")
-		dbgF      = flag.Bool("debug", false, "Log request and response bodies")
-		confPath  = flag.String("conf", "", "config path")
+		hostF = flag.String("host", "localhost", "Server host (valid values: localhost)")
+		//domainF   = flag.String("domain", "", "Host domain name (overrides host domain specified in service design)")
+		portF    = flag.String("port", "8090", "HTTP port (overrides host HTTP port specified in service design)")
+		secureF  = flag.Bool("secure", false, "Use secure scheme (https or grpcs)")
+		dbgF     = flag.Bool("debug", false, "Log request and response bodies")
+		confPath = flag.String("conf", "", "config path")
 	)
 	flag.Parse()
 
@@ -85,35 +85,11 @@ func main() {
 	ctx, cancel := context.WithCancel(context.Background())
 
 	// Start the servers and send errors (if any) to the error channel.
-	switch *hostF {
-	case "localhost":
-		{
-			addr := "http://localhost:8090"
-			u, err := url.Parse(addr)
-			if err != nil {
-				log.Fatal().Msg(fmt.Sprintf("invalid URL %s: %#v", addr, err))
-			}
-			if *secureF {
-				u.Scheme = "https"
-			}
-			if *domainF != "" {
-				u.Host = *domainF
-			}
-			if *httpPortF != "" {
-				h, _, err := net.SplitHostPort(u.Host)
-				if err != nil {
-					log.Fatal().Msg(fmt.Sprintf("invalid URL %s: %#v", u.Host, err))
-				}
-				u.Host = net.JoinHostPort(h, *httpPortF)
-			} else if u.Port() == "" {
-				u.Host = net.JoinHostPort(u.Host, "80")
-			}
-			handleHTTPServer(ctx, u, authEndpoints, hyCompanyEndpoints, healthEndpoints, hyTechEndpoints, hyUserEndpoints, hyUsertechEndpoints, hyUserWorkHistoryEndpoints, &wg, errc, *dbgF, myLogger)
-		}
-
-	default:
-		log.Fatal().Msg(fmt.Sprintf("invalid host argument: %q (valid hosts: localhost)", *hostF))
+	u, err := getURL(*hostF, *portF, *secureF)
+	if err != nil {
+		log.Fatal().Msg(err.Error())
 	}
+	handleHTTPServer(ctx, u, authEndpoints, hyCompanyEndpoints, healthEndpoints, hyTechEndpoints, hyUserEndpoints, hyUsertechEndpoints, hyUserWorkHistoryEndpoints, &wg, errc, *dbgF, myLogger)
 
 	// Wait for signal.
 	log.Info().Msg(fmt.Sprintf("exiting (%v)", <-errc))
@@ -123,4 +99,25 @@ func main() {
 
 	wg.Wait()
 	log.Info().Msg("exited")
+}
+
+func getURL(host, port string, isSecure bool) (*url.URL, error) {
+	addr := fmt.Sprintf("http://%s:%s", host, port)
+	u, err := url.Parse(addr)
+	if err != nil {
+		return nil, errors.Wrapf(err, "invalid URL %s: %#v", addr)
+	}
+	if isSecure {
+		u.Scheme = "https"
+	}
+	//if port != "" {
+	//	h, _, err := net.SplitHostPort(u.Host)
+	//	if err != nil {
+	//		log.Fatal().Msg(fmt.Sprintf("invalid URL %s: %#v", u.Host, err))
+	//	}
+	//	u.Host = net.JoinHostPort(h, port)
+	//} else if u.Port() == "" {
+	//	u.Host = net.JoinHostPort(u.Host, "80")
+	//}
+	return u, nil
 }
